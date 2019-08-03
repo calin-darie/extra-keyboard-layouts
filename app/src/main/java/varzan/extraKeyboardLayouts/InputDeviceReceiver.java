19859fd4118612759a07614b6654149abe61f1a7
Calin Darie <calin_darie@fakeyahoo.com>
2019-08-03 10:37:49 +0300

    buildable android studio project created new project with no activity removed appcompat dependency declared receiver in manifest see docs https://developer.android.com/reference/android/hardware/input/InputManager#ACTION_QUERY_KEYBOARD_LAYOUTS no op receiver apparently required

---
+++ app/src/main/java/varzan/extraKeyboardLayouts/InputDeviceReceiver.java
@@ -0,0 +1,12 @@
+package varzan.extraKeyboardLayouts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class InputDeviceReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
